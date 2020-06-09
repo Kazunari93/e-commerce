52d811e23af1a53e2920c748226f971706dfff19
Kazunari93 <aooni0930@gmail.com>
2020-06-09 22:57:17 +0900

    router

--- clothing/src/components/menu-item/menu-item.component.jsx
+++ clothing/src/components/menu-item/menu-item.component.jsx
@@ -1,19 +1,31 @@
 import React from "react";
+import { withRouter } from "react-router-dom";
+
 import "./menu-item.styles.scss";
 
-export const MenuItem = ({ title, imageUrl, size }) => {
+export const MenuItem = ({
+  title,
+  imageUrl,
+  size,
+  history,
+  linkUrl,
+  match,
+}) => {
   return (
-    <div className={`${size} menu-item`}>
+    <div
+      className={`${size} menu-item`}
+      onClick={() => history.push(`${match.url}${linkUrl}`)}
+    >
       <div
         className="background-image"
         style={{ backgroundImage: `url(${imageUrl})` }}
       />
       <div className="content">
         <h1 className="title">{title.toUpperCase()}</h1>
-        <span className="title">SHOP NOW</span>
+        <span className="subtitle">SHOP NOW</span>
       </div>
     </div>
   );
 };
 
-export default MenuItem;
+export default withRouter(MenuItem);
