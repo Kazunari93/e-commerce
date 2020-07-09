4cd6014e1e0914fd3454588702075e11ea819de4
Kazunari93 <aooni0930@gmail.com>
2020-07-09 22:36:59 +0900

    put multiple same items in the cart

--- clothing/src/components/cart/cart-dropdown.component.jsx
+++ clothing/src/components/cart/cart-dropdown.component.jsx
@@ -1,15 +1,26 @@
 import React from "react";
+// import { connect } from "react-redux";
 import CustomButton from "../custom-button/custom-button.component";
+//import CartItem from "../cart-item/cart-item.component";
 import "./cart-dropdown.styles.scss";
 
-const CartDropDown = () => {
+const CartDropDown = ({ cartItems }) => {
   return (
     <div className="cart-dropdown">
       <div className="cart-items">
-        <CustomButton>GO</CustomButton>
+        <CustomButton>GO</CustomButton>;
       </div>
     </div>
   );
 };
 
+//  {
+//    cartItems.map((cartItem) => <CartItem key={cartItem.id} item={cartItem} />);
+//  }
+
+// const mapStateToProps = ({ cart: { cartItems } }) => ({
+//   cartItems,
+// });
+
+// connect(mapStateToProps);
 export default CartDropDown;
